98f9e5cbc48e4180988c5a1563438576608d4975
naveenkv <naveenkv66@gmail.com>
2019-06-05 17:57:07 +0530

    EmployeeList

---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,12 @@
+export class Employee {
+   name: string;
+   id: number;
+   gender: string;
+   email?: string;
+   phoneNumber?: number;
+   contactPreference: string;
+   dateOfBirth: Date;
+   department: string;
+   isActive: boolean;
+   photoPath?: string;
+}
